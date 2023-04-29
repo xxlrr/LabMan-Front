850a6d267568c17798dbfb6f4d91e4204d433b67
Hongxing Hao <a1845302@adelaide.edu.au>
2023-04-29 18:14:44 +1000

    Add local token operation

---
+++ src/utils/token.js
@@ -0,0 +1,17 @@
+// encapsulate the token interface
+
+const key = "token";
+
+const setToken = (token) => {
+  return window.localStorage.setItem(key, token);
+};
+
+const getToken = () => {
+  return window.localStorage.getItem(key);
+};
+
+const removeToken = () => {
+  return window.localStorage.removeItem(key);
+};
+
+export { setToken, getToken, removeToken };
