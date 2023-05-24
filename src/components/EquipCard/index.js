10904152cde59f162e27f07d50ad716941c5b297
Hongxing Hao <a1845302@adelaide.edu.au>
2023-05-24 14:40:30 +1000

    add EquipCard component

---
+++ src/components/EquipCard/index.js
@@ -0,0 +1,23 @@
+import { Card } from "antd";
+
+export default function EquipCard({equip}) {
+  console.log(equip);
+  if (equip) {
+    return (
+      <Card
+        hoverable
+        style={{ margin: "32px" }}
+        bordered={false}
+        cover={<img style={{ padding: "32px" }} src={equip.photo} />}
+      >
+        <Card.Meta
+          title={equip.name}
+          description={
+            <pre style={{ overflow: "auto" }}>{equip.description}</pre>
+          }
+        />
+      </Card>
+    );
+  }
+  return null;
+}
